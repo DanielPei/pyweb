9176552bb908b33f9d965eade63c108e0d8883b8
DaneilPei <peixq1222@icloud.com>
2018-08-07 00:16:50 +0800

    add django and vue demo

---
+++ django-vue/djangovue/frontend/src/components/Library.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="home">
+		<el-row>
+			<el-table : data='book_list' style="width:100%" border>
+				<el-table-column prop="book_name" label="shuming" min-width="100">
+					<template scope="scope">{{scope.row.fields.book_name}}</template>
+				</el-table-column>
+			</el-table>
+		</el-row>
+	</div>
+</template>
